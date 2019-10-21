8e2af7bb3f532366a315b1c25a77bf505efa3d32
Allison Nakazawa <anakazawa7@gatech.edu>
2019-10-21 16:17:43 -0400

    Added atl_map

---
+++ data_visualization/index.js
@@ -0,0 +1,18 @@
+import 'ol/ol.css';
+import {Map, View} from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+
+const map = new Map({
+  target: 'map',
+  layers: [
+    new TileLayer({
+      source: new OSM()
+    })
+  ],
+  view: new View({
+    projection: 'EPSG:4326',
+    center: [-84.3880,33.7490],
+    zoom: 13
+  })
+});
